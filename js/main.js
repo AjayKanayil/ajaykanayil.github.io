618b04ab8e8c87ba6e704c474bcbb6ef0858acdd
AjayKanayil <kajayknr@gmail.com>
2017-08-03 00:11:27 +0530

    Ajay-image interactions
    Ajay-image interactions

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
+var imagepos = $("#ajay-image").offset();
+var imagesize = $("#ajay-image").width();
 
+$(document).mousemove(function(event){
+    // $("span").text(event.pageX + ", " + event.pageY);
+    $("#ajay-image").css("left",imagepos.left+event.pageX/100);
+    $("#ajay-image").css("top",imagepos.top+event.pageY/100);
 
-$(document).ready(function(){
-	$(body).mousemove(function() {
-		  alert("hello");
-	});
+    // $("#designation").text(imagepos.left+event.pageX/100);
 
-	$(body).click(function() {
-		  alert("hello");
-	});
-})
+    // if(imagepos.left==imagepos.left+event.pageX/50)
+    // {
+    // 	alert("ggg");
+    // }
+    
+});
