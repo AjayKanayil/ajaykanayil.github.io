c147cb5b06ea9d6b26b8fafa78db6e7955ed3116
AjayKanayil <kajayknr@gmail.com>
2017-08-07 08:33:21 +0530

    ajay-image interaction
    ajay-image interaction

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ var imagesize = $("#ajay-image").width();
 
 $(document).mousemove(function(event){
     // $("span").text(event.pageX + ", " + event.pageY);
-    $("#ajay-image").css("left",imagepos.left+event.pageX/100);
-    $("#ajay-image").css("top",imagepos.top+event.pageY/100);
+    $("#ajay-image").css("left",imagepos.left+event.pageX/20);
+    $("#ajay-image").css("top",imagepos.top+event.pageY/20);
 
     // $("#designation").text(imagepos.left+event.pageX/100);
 
